26bed02a4bc527ac50536547c3a7d9c41dee5d3c
R4M45H <R4M45H@users.noreply.github.com>
2020-02-04 02:47:18 +0300

    up slider

--- js/jquery.hislide.js
+++ js/jquery.hislide.js
@@ -3,24 +3,18 @@
         var $ele = $(ele);
         var setting = {
             speed: 500,
-            interval:50000,
+            interval:500000,
             
         };
         $.extend(true, setting, options);
         var states = [
-            { $zIndex: 1, width: 130, height: 150, top: 69, left: 134, $opacity: 1 /* 0.4 */ },
-            { $zIndex: 2, width: 170, height: 170, top: 59, left: 0, $opacity: 1 /* 0.7 */ },
-            { $zIndex: 3, width: 224, height: 218, top: 35, left: 110, $opacity: 1 },
-            { $zIndex: 4, width: 170, height: 288, top: 0, left: 263, $opacity: 1 /* 0.7 */ },
-            { $zIndex: 5, width: 130, height: 218, top: 35, left: 470, $opacity: 1 /* 0.4 */ }
+            { $zIndex: 1, width: 224, height: 388, top: 59, left: 20, $opacity:1 },
+            { $zIndex: 2, width: 250, height: 445, top: 35, left: 100, $opacity: 1 },
+            { $zIndex: 3, width:  332, height: 598, top: -50, left: 200, $opacity: 1 },
+            { $zIndex: 2, width: 250, height: 445, top: 35, left: 370, $opacity: 1 },
+            { $zIndex: 1, width: 224, height: 388, top: 59, left: 500, $opacity: 1 }
         ];
-/*         { $zIndex: 1, width: 120, height: 150, top: 69, left: 134, $opacity: 0.2 },
-        { $zIndex: 2, width: 130, height: 170, top: 59, left: 0, $opacity: 0.4 },
-        { $zIndex: 3, width: 170, height: 218, top: 35, left: 110, $opacity: 0.7 },
-        { $zIndex: 4, width: 224, height: 288, top: 0, left: 263, $opacity: 1 },
-        { $zIndex: 3, width: 170, height: 218, top: 35, left: 470, $opacity: 0.7 },
-        { $zIndex: 2, width: 130, height: 170, top: 59, left: 620, $opacity: 0.4 },
-        { $zIndex: 1, width: 120, height: 150, top: 69, left: 500, $opacity: 0.2 } */
+
         var $lis = $ele.find('li');
         var timer = null;
 
