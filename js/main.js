bdad52d06947dee77dc068876392bad2122a2e6a
R4M45H <R4M45H@users.noreply.github.com>
2020-03-04 16:31:00 +0300

    14

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 $("button").click(function() {
   $(".hamburger").toggleClass("focus");
   $(".content").toggleClass("show");
-});  
-
-
+});
+$("#links li a").click(function() {
+  $(".content").removeClass('show'); 
+  $(".hamburger").removeClass("focus"); 
+   });
